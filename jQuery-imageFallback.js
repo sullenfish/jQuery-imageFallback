eb27af02b4701719db1d71456a1373dbe013c82b
Fred Ostrander <fred@sullenfish.com>
2015-11-23 15:04:18 -0600

    Tidy up whitespace a bit more in jQuery-imageFallback.js

--- jQuery-imageFallback.js
+++ jQuery-imageFallback.js
@@ -1,14 +1,14 @@
 (function($) {
 	var defaultSrc = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAD1BMVEX+/v7///+xsbHl5eX////h369FAAAAAnRSTlMAAHaTzTgAAABgSURBVHgB3dNBCkMxCADROuP9z1xSSpBWfsBlZhXIWym+4tBlgFIP8hvIAaSw/2cglSewegB+mgAwFRQzK9ghLBko7ajZD2IAoJp/gBIgCwHdLkRTEJ2BWgN+uu0uDr0B4XUFsa/5KfQAAAAASUVORK5CYII=';
 	var methods = {
-    	update: function(src, srcList, alternateSrc) {
-    		if (src.length === 0) {
+		update: function(src, srcList, alternateSrc) {
+			if (src.length === 0) {
 				this.src = alternateSrc ? alternateSrc : defaultSrc;
-    		} else {
-    			this.src = src;
-    			$(this).imageFallback(srcList, alternateSrc);
-    		}
-    	}
+			} else {
+				this.src = src;
+				$(this).imageFallback(srcList, alternateSrc);
+			}
+		}
 	};
 	$.fn.imageFallback = function() {
 		var src = ''
@@ -22,14 +22,12 @@
 						do {
 							src = srcList.shift();
 							src = typeof src === 'string' ? src : '';
-							
 						} while (src.length === 0 && srcList.length !== 0);
 					}
 					break;
 				case 'string':
 					src = arguments[0];
 					break;
-				
 			}
 			if (typeof arguments[1] === 'string') {
 				alternateSrc = arguments[1];
