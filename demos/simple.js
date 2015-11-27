11978f5f02ec68cba054197f788c14cae8ab0c25
Fred Ostrander <fred@sullenfish.com>
2015-11-27 08:11:04 -0600

    Add simple jsFiddle demo

---
+++ demos/simple.js
@@ -0,0 +1,12 @@
+var images = $('img');
+
+images.each(function(index, element) {
+	element = $(element);
+	element.one('load', function() {
+		element.next('.result').append('Loaded.<br/>');
+	});
+});
+
+images.eq(1).imageFallback();
+
+images.eq(2).imageFallback('/img/logo.png');
